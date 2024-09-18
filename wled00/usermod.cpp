5c3674d09d8d32005e77a4b28e972a8cb2597ab1
Marc LANDES <marclandes@outlook.com>
2024-09-18 10:59:20 +0200

    added new usermod to control preset from buttons inputs
    (thanks to https://github.com/Aircoookie/WLED/blob/main/usermods/battery_keypad_controller/wled06_usermod.ino)

--- wled00/usermod.cpp
+++ wled00/usermod.cpp
@@ -1,29 +1,40 @@
 #include "wled.h"
-/*
- * This v1 usermod file allows you to add own functionality to WLED more easily
- * See: https://github.com/Aircoookie/WLED/wiki/Add-own-functionality
- * EEPROM bytes 2750+ are reserved for your custom use case. (if you extend #define EEPSIZE in const.h)
- * If you just need 8 bytes, use 2551-2559 (you do not need to increase EEPSIZE)
- *
- * Consider the v2 usermod API if you need a more advanced feature set!
- */
-
 //Use userVar0 and userVar1 (API calls &U0=,&U1=, uint16_t)
 
-//gets called once at boot. Do all initialization that doesn't depend on network here
-void userSetup()
-{
+unsigned long lastTime = 0;
+int delayMs = 10;
+const int pinA = 5;
+const int pinB = 0;
+int oldA = LOW, oldB = LOW;
 
+//gets called once at boot. Do all initialization that doesn't depend on network here
+void userSetup() {
+  pinMode(pinA, INPUT_PULLUP);
+  pinMode(pinB, INPUT_PULLUP);
+  oldA = digitalRead(pinA);
+  oldB = digitalRead(pinB);
 }
 
 //gets called every time WiFi is (re-)connected. Initialize own network interfaces here
-void userConnected()
-{
-
+void userConnected() {
 }
 
 //loop. You can use "if (WLED_CONNECTED)" to check for successful connection
-void userLoop()
-{
+void userLoop() {
+  if (millis()- lastTime > delayMs) {
+    int A = digitalRead(pinA);
+    int B = digitalRead(pinB);
 
+    if (A == LOW && B == HIGH) {
+      applyPreset(1);
+    }
+    else if (A == HIGH && B == LOW) {
+      applyPreset(2);
+    }
+    else{
+      applyPreset(3);
+    }
+
+    lastTime = millis();
+  }
 }
